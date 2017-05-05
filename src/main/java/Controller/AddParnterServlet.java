d9127af5a5c159a72140c5b542ee734d1720f92c
karol kolanczak <karol.kolanczak@hotmail.com>
2017-05-05 16:09:45 +0200

    showing list of Partner on admin panel and adding new ones (not db only sourced from servlet on the view)

---
+++ src/main/java/Controller/AddParnterServlet.java
@@ -0,0 +1,65 @@
+package Controller;
+
+import Model.Partner;
+import Model.PartnerDataUtil;
+import Model.User;
+import Model.UserDataUtil;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@WebServlet("/AddParnterServlet")
+public class AddParnterServlet extends HttpServlet {
+
+    Partner partner;
+    PartnerDataUtil partnerDataUtil;
+    List<Partner> listOfPartners;
+    Session sessionClass;
+    HttpSession session;
+
+    public AddParnterServlet() {
+        partner = new Partner();
+        sessionClass = new Session();
+        partnerDataUtil=new PartnerDataUtil();
+        listOfPartners=new ArrayList<>();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("----------- FROM ADD PARTNER SERVLET -----------");
+
+        session=sessionClass.getSession(request);
+
+        System.out.println(request.getParameter("firstName"));
+
+        partner.setFirstName(request.getParameter("firstName"));
+        partner.setLastName(request.getParameter("lastName"));
+//        partner.setLogin(request.getParameter("login"));
+//        partner.setPassword(request.getParameter("password"));
+        partner.setEmail(request.getParameter("email"));
+//        partner.setJoinedDate(((Date)request.getParameter("joinedDate")));
+
+        listOfPartners=((List<Partner>) session.getAttribute("listOfPartners"));
+        listOfPartners=partnerDataUtil.addToListOfPartners(listOfPartners, partner);
+
+        session.setAttribute("listOfPartners",listOfPartners);
+
+//        System.out.println("FROM SESSION - LOGIN "+((User)session.getAttribute("userSessionData")).getLogin());
+        System.out.println("------------------");
+
+
+        RequestDispatcher dispatcher=request.getRequestDispatcher("/admin.jsp");
+        dispatcher.forward(request,response);
+    }
+
+
+}
